dbd4919ba381c26c61b05507266ec609c03c325b
lpnh <paniguel.lpnh@gmail.com>
2025-02-05 20:18:54 -0300

    feat: add metadata preview, improve bash support

--- main.lua
+++ main.lua
@@ -1,29 +1,45 @@
 local shell = os.getenv("SHELL"):match(".*/(.*)")
 
+-- mimic bat grid,header style
 local bar =
 	[[echo -e "\x1b[38;2;148;130;158m────────────────────────────────────────────────────────────────────────────────\x1b[m";]]
 local bar_n =
 	[[echo -e "\n\x1b[38;2;148;130;158m────────────────────────────────────────────────────────────────────────────────\x1b[m";]]
 local dir_name = [[echo -ne "Dir: \x1b[1m\x1b[38m{}\x1b[m";]]
-local bat_prev = "bat --color=always --style=snip,grid,header {}'"
 local is_empty_dir = [[test -z "$(eza -A {})" && echo -ne "  <EMPTY>\n" || ]]
-local long_flags = "--git --git-repos --header --long --mounts --no-user --octal-permissions --total-size"
-local eza_cmd = "eza " .. long_flags .. " --color=always --group-directories-first --icons {};"
+local echo_meta =
+	[[test -d {} && echo -ne "Dir: \x1b[1m\x1b[38m{}\x1b[m  <METADATA>" || echo -ne "File: \x1b[1m\x1b[38m{}\x1b[m  <METADATA>";]]
+
+-- bat preview
+local bat_prev = "bat --color=always --style=grid,header {}'"
+
+-- eza preview
+local eza_flags =
+	" --git --git-repos --header --long --mounts --no-user --octal-permissions --total-size --color=always --icons {} "
+local eza_cmd = "eza --group-directories-first" .. eza_flags .. [[ | sed "s/\x1b\[4m//g; s/\x1b\[24m//g";]]
 local eza_tbl = {
+	default = "(" .. bar .. dir_name .. is_empty_dir .. bar_n .. eza_cmd .. bar .. ")",
 	fish = "begin; " .. bar .. dir_name .. is_empty_dir .. bar_n .. eza_cmd .. bar .. " end",
 }
+local eza_prev = eza_tbl[shell] or eza_tbl.default
+
+-- ctrl-space metadata preview
+local eza_list_dirs = "eza --list-dirs" .. eza_flags .. [[ | sed "s/\x1b\[4m//g; s/\x1b\[24m//g"']]
+local eza_bind_prev = " --bind 'ctrl-space:preview:" .. bar .. echo_meta .. bar_n .. eza_list_dirs
+
+-- fzf
 local fzf_cmd = "fzf --reverse --no-multi --preview-window=up,60%"
-local preview = " --preview='test -d {} && " .. eza_tbl[shell] .. " || " .. bat_prev
+local preview = " --preview='test -d {} && " .. eza_prev .. " || " .. bat_prev
 local fd_all_tbl = {
-	default = "(fd --type=d; fd --type=f)",
-	fish = "begin; echo ../; echo .; fd --type d; fd --type f; end",
+	default = [[(echo ../; echo ./; fd --type=d; fd --type=f)]],
+	fish = "begin; echo ../; echo .; fd --type=d; fd --type=f; end",
 }
 local fd_all = fd_all_tbl[shell] or fd_all_tbl.default
 local fd_cmd_from = {
 	all = fd_all,
 	cwd = "fd --max-depth=1",
-	dir = "fd --type=d",
-	file = "fd --type=f",
+	dir = "fd --type=dir",
+	file = "fd --type=file",
 }
 
 local fail = function(s, ...) ya.notify { title = "fdp", content = string.format(s, ...), timeout = 5, level = "error" } end
@@ -31,7 +47,7 @@ local fail = function(s, ...) ya.notify { title = "fdp", content = string.format
 local function entry(_, job)
 	local _permit = ya.hide()
 	local fd_cmd = fd_cmd_from[job.args[1]]
-	local args = fd_cmd .. " | " .. fzf_cmd .. preview
+	local args = fd_cmd .. " | " .. fzf_cmd .. preview .. eza_bind_prev
 
 	local child, err =
 		Command(shell):args({ "-c", args }):stdin(Command.INHERIT):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
