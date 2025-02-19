f1da34c6944df142038123f1816cae754fc6af41
lpnh <paniguel.lpnh@gmail.com>
2025-02-19 10:43:26 -0300

    feat: allow user customization

--- main.lua
+++ main.lua
@@ -6,11 +6,11 @@ local fail = function(s, ...) ya.notify { title = "fdp", content = string.format
 -- shell compatibility
 local sh_compat = {
 	default = {
-		wrap_cmd = function(cmd) return "(" .. cmd .. ")" end,
+		wrap = function(cmd) return "(" .. cmd .. ")" end,
 		logic = { cond = "[[ ! $FZF_PROMPT =~ fd ]] &&", op = "||" },
 	},
 	fish = {
-		wrap_cmd = function(cmd) return "begin; " .. cmd .. "; end" end,
+		wrap = function(cmd) return "begin; " .. cmd .. "; end" end,
 		logic = { cond = 'not string match -q "*fd*" $FZF_PROMPT; and', op = "; or" },
 	},
 }
@@ -24,16 +24,15 @@ local bar_n =
 	[[echo -e "\n\x1b[38;2;148;130;158m────────────────────────────────────────────────────────────────────────────────\x1b[m";]]
 
 -- preview
-local bat_prev = "bat --color=always --style=grid,header {}"
-local eza_prev = function(prev)
+local eza_prev = function(prev, opts)
 	local name = {
 		default = [[echo -ne "Dir: \x1b[1m\x1b[38m{}\x1b[m";]],
 		meta = [[test -d {} && echo -ne "Dir: \x1b[1m\x1b[38m{}\x1b[m"]]
 			.. [[ || echo -ne "File: \x1b[1m\x1b[38m{}\x1b[m";]],
 	}
 	local extra_flags = {
-		default = "--oneline",
-		meta = "--git --git-repos --header --long --mounts --no-user --octal-permissions --total-size",
+		default = "--oneline " .. opts.eza,
+		meta = "--git --git-repos --header --long --mounts --no-user --octal-permissions " .. opts.eza_meta,
 	}
 
 	return table.concat({
@@ -47,22 +46,45 @@ local eza_prev = function(prev)
 		bar,
 	}, " ")
 end
-local default_prev = string.format("test -d {} && %s || %s", sh.wrap_cmd(eza_prev("default")), bat_prev)
 
 -- bind toggle fzf match
 local bind_match_tmpl = "--bind='ctrl-s:transform:%s "
 	.. [[echo "rebind(change)+change-prompt(fd> )+clear-query+reload:%s" %s ]]
 	.. [[echo "unbind(change)+change-prompt(fzf> )+clear-query"']]
 
+-- handle user custom options
+local fmt_opts = function(opt)
+	if type(opt) == "string" then
+		return " " .. opt
+	elseif type(opt) == "table" then
+		return " " .. table.concat(opt, " ")
+	end
+	return ""
+end
+
+local get_custom_opts = ya.sync(function(self)
+	local opts = self.custom_opts or {}
+
+	return {
+		fzf = fmt_opts(opts.fzf),
+		fd = fmt_opts(opts.fd),
+		bat = fmt_opts(opts.bat),
+		eza = fmt_opts(opts.eza),
+		eza_meta = fmt_opts(opts.eza_meta),
+	}
+end)
+
 -- fzf
-local fzf_from = function(job_args)
+local fzf_from = function(job_args, opts)
 	local cmd_tbl = {
-		all = sh.wrap_cmd("fd --type=d {q}; fd --type=f {q}"),
-		cwd = sh.wrap_cmd("fd --max-depth=1 --type=d {q}; fd --max-depth=1 --type=f {q}"),
-		dir = "fd --type=dir {q}",
-		file = "fd --type=file {q}",
+		all = sh.wrap("fd --type=d " .. opts.fd .. " {q}; fd --type=f " .. opts.fd .. " {q}"),
+		cwd = sh.wrap("fd --max-depth=1 --type=d " .. opts.fd .. " {q}; fd --max-depth=1 --type=f " .. opts.fd .. " {q}"),
+		dir = "fd --type=dir " .. opts.fd .. " {q}",
+		file = "fd --type=file " .. opts.fd .. " {q}",
 	}
 	local fd_cmd = cmd_tbl[job_args]
+	local bat_prev = "bat --color=always " .. opts.bat .. " {}"
+	local default_prev = string.format("test -d {} && %s || %s", sh.wrap(eza_prev("default", opts)), bat_prev)
 
 	local fzf_tbl = {
 		"fzf",
@@ -78,17 +100,30 @@ local fzf_from = function(job_args)
 		"--bind='ctrl-]:change-preview-window(80%|66%)'",
 		"--bind='ctrl-\\:change-preview-window(right|up)'",
 		string.format("--bind 'alt-c:change-preview-label(content)+change-preview:%s'", default_prev),
-		string.format("--bind 'alt-m:change-preview-label(metadata)+change-preview:%s'", eza_prev("meta")),
+		string.format("--bind 'alt-m:change-preview-label(metadata)+change-preview:%s'", eza_prev("meta", opts)),
 		string.format(bind_match_tmpl, sh.logic.cond, fd_cmd, sh.logic.op),
-		-- opts_tbl.fzf,
+		opts.fzf,
 	}
 
 	return table.concat(fzf_tbl, " ")
 end
 
+local function setup(self, opts)
+	opts = opts or {}
+
+	self.custom_opts = {
+		fzf = opts.fzf,
+		fd = opts.fd,
+		bat = opts.bat,
+		eza = opts.eza,
+		eza_meta = opts.eza_meta,
+	}
+end
+
 local function entry(_, job)
 	local _permit = ya.hide()
-	local args = fzf_from(job.args[1])
+	local custom_opts = get_custom_opts()
+	local args = fzf_from(job.args[1], custom_opts)
 
 	local child, err =
 		Command(shell):args({ "-c", args }):stdin(Command.INHERIT):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
@@ -116,4 +151,4 @@ local function entry(_, job)
 	end
 end
 
-return { entry = entry }
+return { entry = entry, setup = setup }
